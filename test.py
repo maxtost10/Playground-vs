24fdcf2b0bc99933b5073703f812a0bc13139e90
“Max <m.luke.tost@gmail.com>
2024-11-19 20:26:01 +0100

    First commit

---
+++ test.py
@@ -0,0 +1,18 @@
+a = 33
+b = 200
+
+def printerbuddy(x):
+    print(x)
+
+
+if b > a:
+    print('b is greater thatn a')
+    print(b)
+
+b = 20
+a = 100
+
+if a > b:
+    printerbuddy('Now a is greater than b')
+
+print('Hi')
